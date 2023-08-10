67d9204ba0e98f6bbf8ccc0dc0cdc3e346820185
Seth Yoo <syoo881@aucklanduni.ac.nz>
2023-08-10 22:54:07 +1200

    implementing paper scissors rock game

---
+++ src/main/java/nz/ac/auckland/se206/controllers/VaseGameController.java
@@ -0,0 +1,25 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class VaseGameController {
+
+  @FXML private Label cadeScore;
+
+  @FXML private ImageView computer;
+
+  @FXML private Button paperButton;
+
+  @FXML private ImageView player;
+
+  @FXML private Label result;
+
+  @FXML private Button rockButton;
+
+  @FXML private Button scissorButton;
+
+  @FXML private Label userScore;
+}
